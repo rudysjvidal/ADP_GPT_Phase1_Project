dbffd71f9f2f7c940992d316bbc0ea0099254774
Rudys J Vidal <rvid1019@gmail.com>
2025-08-10 18:15:17 -0400

    implemented functionality for adding customers

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import CustomerList from './components/CustomerList';
+import AddCustomerForm from './components/AddCustomerForm';
+
 
 function App() {
   return (
@@ -8,7 +11,10 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route index element={<CustomerList />} />
+            <Route path="add" element={<AddCustomerForm />} />
+          </Route>
         </Routes>
       </div>
     </Router>
