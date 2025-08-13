0032437b46744f55f2db53597d7eb19a2ad4bfb2
Alex Hai <alex.hai@adp.com>
2025-08-13 10:43:40 -0400

    opening access permissions for preview

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: 'all',
+  },
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: 'all',
+  },
   plugins: [react()],
   server: {
     port: 3000,
