9635720b06571e4aced9852f6ec38b1b186e7eae
Alex Hai <alex.hai@adp.com>
2025-08-13 11:06:45 -0400

    another try for render deployment

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 4173,
-    allowedHosts: ['adp-customer-dashboard-react-project.onrender.com', '.onrender.com', 'localhost']
-  },
+  plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    allowedHosts: ['adp-customer-dashboard-react-project.onrender.com', '.onrender.com', 'localhost']
-  },
-  plugins: [react()]
-})
+    port: 3000,
+    open: true
+  }
+});
